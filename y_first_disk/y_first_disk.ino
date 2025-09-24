e581b746eb41383ad72938ccbef3d2d336975909
YYYYUKICHI <tmrmeme7@gmail.com>
2025-09-24 22:01:21 +0900

    Update y_first_disk.ino

--- y_first_disk/y_first_disk.ino
+++ y_first_disk/y_first_disk.ino
@@ -10,7 +10,7 @@ unsigned long lastUpdate = 0;
 int currentIndex = 0;
 bool animationFinished = false;
 #define LED_on_wait  68500
-#define LED_disk_speed  50
+#define LED_disk_speed  20
 #define LED_off_wait  4000
 int test = 2;//1:本モード,2:開発モード
 
@@ -21,24 +21,27 @@ void setup() {
 }
 
 void loop() {
-  delay(LED_on_wait);
-  LEDdisk_on(NUM_LEDS,LED_disk_speed);
-  delay(LED_off_wait);
-  setAllColor(ring.Color(0, 0, 0));
-  delay(10000000);
+  
+
+         delay(LED_on_wait);
+         LEDdisk_on(NUM_LEDS,LED_disk_speed);
+         delay(LED_off_wait);
+         setAllColor(ring.Color(0, 0, 0));
+         delay(10000000);
+
 }
 
 void setAllColor(uint32_t color) {
-  for (uint16_t i = 0; i <NUM_LEDS; i++) {
-    ring.setPixelColor(i, color);
+    for (uint16_t i = 0; i <NUM_LEDS; i++) {
+      ring.setPixelColor(i, color);
+    }
+      ring.show();
   }
-  ring.show();
-}
 
 void LEDdisk_on(uint32_t num,uint32_t speed){
-  for(uint16_t i=0; i<=num; i++) {
-    ring.setPixelColor(i, ring.Color(255, 255, 255)); // 白に点灯
-    ring.show();
-    delay(speed);
-  }
+           for(uint16_t i=0; i<=num; i++) { //armLeft_LED=bodyRigh_LED　かつarmLeft＿LEDが一番個数が多いためarmLeft_LEDを使用
+                 ring.setPixelColor(i, ring.Color(255, 255, 255)); // 白に点灯
+                 ring.show();
+            delay(speed);
+               }
 }
